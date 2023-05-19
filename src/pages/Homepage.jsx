8e21651d154a511af7b32b4ed5a214c04949f044
ranuaa <white_angleable@yahoo.com>
2023-05-19 21:13:17 +0700

    startinginitial

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return <div>
+    <h1>Nama : Ranu Agita Fahmi</h1>
+    <h1>Umur : 27 Tahun</h1>
+    <h1>Almat : Solo, Jawa Tengah</h1>
+  </div>;
+};
+
+export default Homepage;
